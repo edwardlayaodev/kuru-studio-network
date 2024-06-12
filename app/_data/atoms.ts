3ce886618044566148fc472f0add045066ec789d
Edward Layao <107934769+edwardlayaodev@users.noreply.github.com>
2024-06-12 20:21:19 +0800

    add userAtom to jotai atoms

--- app/_data/atoms.ts
+++ app/_data/atoms.ts
@@ -1,4 +1,7 @@
 "use client";
+import { User } from "firebase/auth";
 import { atom } from "jotai";
 
 export const msgAtom = atom("hello world");
+
+export const userAtom = atom<User | null>(null);
