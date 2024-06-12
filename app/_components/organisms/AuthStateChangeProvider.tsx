daba2b3c2d9fa19281f51e1b4daea56a7c349085
Edward Layao <107934769+edwardlayaodev@users.noreply.github.com>
2024-06-12 20:20:29 +0800

    Create AuthStateChangeProvider.tsx
    observer for firebase auth state changes

---
+++ app/_components/organisms/AuthStateChangeProvider.tsx
@@ -0,0 +1,33 @@
+"use client";
+import { onAuthStateChanged } from "firebase/auth";
+import { useAtom } from "jotai";
+import { userAtom } from "@/app/_data/atoms";
+import { useEffect } from "react";
+import { auth } from "@/app/_utilities/firebaseConfig";
+import { ReactNode, Fragment as ReactFragment } from "react";
+
+interface IAuthProviderProps {
+  children: ReactNode;
+}
+
+const AuthStateChangeProvider: React.FunctionComponent<IAuthProviderProps> = ({
+  children,
+}) => {
+  const [user, setUser] = useAtom(userAtom);
+
+  // on mount, listen to firebase auth changes
+  useEffect(() => {
+    onAuthStateChanged(auth, (user: any) => {
+      console.log("auth state changed");
+      if (user) {
+        setUser(user);
+      } else {
+        setUser(null);
+      }
+    });
+  }, []);
+
+  return <ReactFragment>{children}</ReactFragment>;
+};
+
+export default AuthStateChangeProvider;
